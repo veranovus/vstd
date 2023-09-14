f7c8c9462ffbbff9ef414b03f9d2be82756a46ce
Batuhan Yigit <batuhanyigit1705@gmail.com>
2023-09-14 18:56:11 +0300

    vstd_string complete

--- _vstd.h
+++ _vstd.h
@@ -35,6 +35,7 @@
 
 #include <inttypes.h>
 #include <memory.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -63,6 +64,8 @@ typedef double f64;
 #ifndef VSTD_INLINE
 #define VSTD_INLINE __attribute__((unused, always_inline)) static inline
 #endif
+
+#define VSTD_STRINGIFY(name) #name
 
 /*****************************************************************************
  *
@@ -101,6 +104,15 @@ typedef struct _VSTD_String VSTD_String;
 #define VSTD_STRING_INITIAL_CAP 1
 #endif
 
+#define VSTD_STRING_EQUAL 0
+
+/*****************************************************************************
+ *
+ * @section
+ *   VSTD String function declarations
+ *
+ * */
+
 /*****************************************************************************
  *
  * @function
@@ -114,7 +126,7 @@ typedef struct _VSTD_String VSTD_String;
  *   New empty VSTD_String with capacity of VSTD_STRING_INITIAL_CAP.
  *
  * */
-VSTD_STATIC _VSTD_String vstd_string_new();
+VSTD_INLINE _VSTD_String vstd_string_new();
 
 /*****************************************************************************
  *
@@ -236,7 +248,7 @@ VSTD_STATIC _VSTD_String *vstd_string_push_str(_VSTD_String *string,
  *
  * @description
  *   Tries to remove the given C string if it is present in the given
- *   VSTD_String.
+ *   VSTD_String. Substring is only removed once.
  *
  * @param[in]
  *   string : VSTD_String to modify.
@@ -274,12 +286,245 @@ VSTD_STATIC _VSTD_String *vstd_string_remove(_VSTD_String *string,
 VSTD_STATIC _VSTD_String *vstd_string_remove_at(_VSTD_String *string,
                                                 usize index, usize len);
 
-VSTD_STATIC char *vstd_string_find_first(const _VSTD_String *string,
+/*****************************************************************************
+ *
+ * @function
+ *   vstd_string_find_first
+ *
+ * @description
+ *   Finds the first occurrence of the given C string inside of the given
+ *   VSTD_String. Underlying implementation of this function just calls strstr
+ *   from standard library. Returns a NULL pointer if it fails.
+ *
+ * @param[in]
+ *   string : VSTD_String to search.
+ * @param[in]
+ *   sub : Null terminated C string to search for.
+ *
+ * @return
+ *   Pointer to the start of the C string's first occurrence inside the given
+ *   VSTD_String or a NULL pointer.
+ *
+ * */
+VSTD_INLINE char *vstd_string_find_first(const _VSTD_String *string,
                                          const char *sub);
+
+/*****************************************************************************
+ *
+ * @function
+ *   vstd_string_find_first
+ *
+ * @description
+ *   Finds the last occurrence of the given C string inside of the given
+ *   VSTD_String. Returns a NULL pointer if it fails.
+ *
+ * @param[in]
+ *   string : VSTD_String to search.
+ * @param[in]
+ *   sub : Null terminated C string to search for.
+ *
+ * @return
+ *   Pointer to the start of the C string's last occurrence inside the given
+ *   VSTD_String or a NULL pointer.
+ *
+ * */
 VSTD_STATIC char *vstd_string_find_last(const _VSTD_String *string,
                                         const char *sub);
-VSTD_STATIC isize vstd_string_compare(const _VSTD_String *string,
+
+/*****************************************************************************
+ *
+ * @function
+ *   vstd_string_compare
+ *
+ * @description
+ *   Compares the given C string with the given VSTD_String. This function is
+ *   only there for API consistency, underlying implementation of this function
+ *   just calls strcmp thus behaviour is identical to that function. For more
+ *   information about strcmp you can check:
+ *   https://pubs.opengroup.org/onlinepubs/9699919799.2016edition/functions/strcmp.html
+ *
+ * @param[in]
+ *   string : VSTD_String to compare.
+ * @param[in]
+ *   str : Null terminated C string to be compared with.
+ *
+ * @return
+ *   Integer value indicating the relationship between the two strings.
+ *
+ * */
+VSTD_INLINE isize vstd_string_compare(const _VSTD_String *string,
                                       const char *str);
+
+/*****************************************************************************
+ *
+ * @function
+ *   vstd_string_free
+ *
+ * @description
+ *   Frees the memory allocated for given VSTD_String's underlying pointer.
+ *
+ * @param[in]
+ *   string : VSTD_String to free.
+ *
+ * */
 VSTD_STATIC void vstd_string_free(_VSTD_String *string);
+
+/*****************************************************************************
+ *
+ * @function
+ *   _vstd_string_realloc
+ *
+ * @description
+ *   Doubles the given VSTD_String's capacity and reallocates the underlying
+ *   pointer. This is a helper function and it should only be used by the vstd
+ *   library functions.
+ *
+ * @param[in]
+ *   string : VSTD_String to reallocate.
+ *
+ * */
+VSTD_INLINE void _vstd_string_realloc(_VSTD_String *string);
+
+/*****************************************************************************
+ *
+ * @section
+ *   VSTD String function implementations
+ *
+ * */
+
+VSTD_INLINE _VSTD_String vstd_string_new() {
+  return vstd_string_with_capacity(VSTD_STRING_INITIAL_CAP);
+}
+
+VSTD_STATIC _VSTD_String vstd_string_with_capacity(usize cap) {
+  return (_VSTD_String){
+      .ptr = (char *)calloc(cap, sizeof(char)),
+      .cap = cap,
+      .len = 0,
+  };
+}
+
+VSTD_STATIC _VSTD_String vstd_string_from(const char *str) {
+  usize len = strlen(str);
+  _VSTD_String s = vstd_string_with_capacity(len + 1);
+
+  strcpy(s.ptr, str);
+  s.len = len;
+
+  return s;
+}
+
+VSTD_STATIC _VSTD_String vstd_string_clone(const _VSTD_String *string) {
+  _VSTD_String clone = vstd_string_with_capacity(string->cap);
+
+  memcpy(clone.ptr, string->ptr, sizeof(char) * string->len);
+  clone.len = string->len;
+
+  return clone;
+}
+
+VSTD_STATIC _VSTD_String vstd_string_format(const char *fmt, ...) {
+  va_list arg_ptr;
+  va_start(arg_ptr, fmt);
+
+  usize len = vsnprintf(NULL, 0, fmt, arg_ptr);
+  _VSTD_String s = vstd_string_with_capacity(len + 1);
+
+  vsnprintf(s.ptr, s.cap, fmt, arg_ptr);
+  s.len = len;
+
+  va_end(arg_ptr);
+  return s;
+}
+
+VSTD_STATIC _VSTD_String *vstd_string_push(_VSTD_String *string, char c) {
+  if (string->len + 1 >= string->cap) {
+    _vstd_string_realloc(string);
+  }
+
+  string->ptr[string->len] = c;
+  string->len++;
+
+  string->ptr[string->len] = '\0';
+  return string;
+}
+
+VSTD_STATIC _VSTD_String *vstd_string_push_str(_VSTD_String *string,
+                                               const char *str) {
+  usize len = strlen(str);
+
+  while (string->len + len >= string->cap) {
+    _vstd_string_realloc(string);
+  }
+
+  strcpy(string->ptr + string->len, str);
+  string->len += len;
+
+  string->ptr[string->len] = '\0';
+  return string;
+}
+
+VSTD_STATIC _VSTD_String *vstd_string_remove(_VSTD_String *string,
+                                             const char *sub) {
+  if (!sub[0]) {
+    return string;
+  }
+
+  usize len = strlen(sub);
+  char *ptr = strstr(string->ptr, sub);
+  if (!ptr) {
+    return string;
+  }
+
+  return vstd_string_remove_at(string, (usize)(ptr - string->ptr), len);
+}
+
+VSTD_STATIC _VSTD_String *vstd_string_remove_at(_VSTD_String *string,
+                                                usize index, usize len) {
+  usize move_size = string->len - (index + len);
+
+  if (move_size > 0) {
+    memmove(string->ptr + index, string->ptr + index + len, move_size);
+  }
+  string->len -= len;
+
+  string->ptr[string->len] = '\0';
+  return string;
+}
+
+VSTD_INLINE char *vstd_string_find_first(const _VSTD_String *string,
+                                         const char *sub) {
+  return strstr(string->ptr, sub);
+}
+
+VSTD_STATIC char *vstd_string_find_last(const _VSTD_String *string,
+                                        const char *sub) {
+  if (!sub[0]) {
+    return NULL;
+  }
+
+  char *pos = NULL;
+  for (char *ptr = string->ptr; (ptr = strstr(ptr, sub)) != NULL; ptr += 1) {
+    pos = ptr;
+  }
+
+  return pos;
+}
+
+VSTD_INLINE isize vstd_string_compare(const _VSTD_String *string,
+                                      const char *str) {
+  return strcmp(string->ptr, str);
+}
+
+VSTD_STATIC void vstd_string_free(_VSTD_String *string) {
+  free(string->ptr);
+  string->len = 0;
+  string->cap = 0;
+}
+
+VSTD_INLINE void _vstd_string_realloc(_VSTD_String *string) {
+  string->cap *= 2;
+  string->ptr = (char *)realloc(string->ptr, sizeof(char) * string->cap);
+}
 
 #endif // VSTD_H_
